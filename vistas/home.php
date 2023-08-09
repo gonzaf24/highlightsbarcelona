0805fb29c9cf582574459693bb05582571c44a01
gonzaf21@hotmail.com <gonzaf21@hotmail.com>
2023-08-10 00:01:34 +0200

    footer

--- vistas/home.php
+++ vistas/home.php
@@ -4,6 +4,6 @@
       <p>Discover Barcelona like never before! We are your trusted guide to uncovering the best places and experiences in the city. At Highlights Barcelona, we take pride in providing you with authentic and useful information, free from ads, so you can make the most of your visit.</p>
       <p>From the most iconic neighborhoods to the picturesque beaches, from must-see attractions to hidden local gems, we have everything you need to enjoy an authentic Barcelona experience.</p>
       <p>Browse through our categories to unlock the city's best-kept secrets. We invite you to explore every corner and indulge in the treasures that Barcelona has to offer.</p>
-      <span class="explore-link">Explore Categories</span>
+      <button class="explore-link">Explore Categories</button>
     </div>
 </section> 
